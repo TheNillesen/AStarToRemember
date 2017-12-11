b635c45353c2fec1d145c20015ce1297f37e89c6
Anders Vestergaard <mufas.anders@hotmail.com>
2017-12-11 11:03:03 +0100

    Base

---
+++ aStarLooksForKeys/aStarLooksForKeys/Map.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aStarLooksForKeys
+{
+    class Map
+    {
+        public Node[,] nodes;
+
+        public Map(int mapWidth, int mapHeight)
+        {
+            nodes = new Node[mapWidth, mapHeight];
+            GenerateMap(mapWidth, mapHeight);
+        }
+
+        private void GenerateMap(int mapWidth, int mapHeight)
+        {
+            for (int i = 0; i < mapWidth; i++)
+            {
+                for(int j = 0; j < mapHeight; i++)
+                {
+                    Node[i, j] = new Node(MyType.walkable);
+                }
+            }
+        }
+    }
+}
---
+++ aStarLooksForKeys/aStarLooksForKeys/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aStarLooksForKeys
+{
+    class Node
+    {
+        public MyType myType;
+
+        public string symbol;
+
+        public Node(MyType myType)
+        {
+            this.myType = myType;
+        }
+
+
+    }
+}
