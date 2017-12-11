75dd17192e271f1e409a0b373f0797a20cd38d50
Anders Vestergaard <mufas.anders@hotmail.com>
2017-12-11 13:42:59 +0100

    Colors

---
+++ aStarLooksForKeys/aStarLooksForKeys/Wizard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aStarLooksForKeys
+{
+    class Wizard
+    {
+        private Position position;
+
+        public Wizard(Position startPosition)
+        {
+            this.position = startPosition;
+        }
+
+        public void Move()
+        {
+
+        }
+    }
+}
