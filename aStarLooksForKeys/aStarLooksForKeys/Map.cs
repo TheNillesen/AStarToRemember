485d22f43471a6576eda5d0c0740e993c1e26bff
Nicklas Nielsen <nick465e@student.eadania.dk>
2017-12-13 13:20:30 +0100

    cloning problemer

--- aStarLooksForKeys/aStarLooksForKeys/Map.cs
+++ aStarLooksForKeys/aStarLooksForKeys/Map.cs
@@ -94,7 +94,7 @@ namespace aStarLooksForKeys
                     Console.ForegroundColor = ConsoleColor.White;
                 }
                 Console.Write("\n");
-            }
+            }//cloningProblemer
         }
     }
 }
