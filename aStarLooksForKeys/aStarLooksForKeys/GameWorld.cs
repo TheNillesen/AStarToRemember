70f0c498ec48936a1f90e98c546948c0afff3191
Anders Vestergaard <mufas.anders@hotmail.com>
2017-12-14 14:13:43 +0100

    Blah

--- aStarLooksForKeys/aStarLooksForKeys/GameWorld.cs
+++ aStarLooksForKeys/aStarLooksForKeys/GameWorld.cs
@@ -17,20 +17,34 @@ namespace aStarLooksForKeys
 
         public GameWorld()
         {
-            Console.SetWindowSize(80, 21);
-
-            Console.Clear();
-            Console.WriteLine("Welcome to the Algorithm Simulator 5000");
-            Console.WriteLine("How will you proceed?");
-            Console.WriteLine("Press 1 for: A*. Press 2 for something else");
-            Console.ReadKey();
-            Console.Clear();
-
             map = new Map(10, 10);
             wizard = new Wizard(map.nodes[8, 1]);
             map.Render(false);
             gameRun = true;
             run = true;
+
+            Console.SetWindowSize(80, 21);
+            Console.Clear();
+            Console.WriteLine("Welcome to the Algorithm Simulator 5000");
+            Console.WriteLine("How will you proceed?");
+            Console.WriteLine("Press 1 for: A*. \nPress 2 for Dijkstra. \nPress 3 to exit game.");
+            ConsoleKeyInfo result = Console.ReadKey();
+            if (result.Key == ConsoleKey.D1)
+            {
+                gameRun = true;
+                wizard.aStar = true;
+                wizard.dijkstra = false;
+            }
+            if (result.Key == ConsoleKey.D2)
+            {
+                gameRun = true;
+                wizard.aStar = false;
+                wizard.dijkstra = true;
+            }
+            if (result.Key == ConsoleKey.D3)
+                run = false;
+            Console.Clear();
+
             GameLoop();
         }
 
@@ -45,18 +59,28 @@ namespace aStarLooksForKeys
 
                 Console.WriteLine("\nYour run of the Algorithm Simulator 5000 have concluded");
                 Console.WriteLine("How will you proceed?");
-                Console.WriteLine("Press 1 for: A*. \nPress 2 for something else. \nPress 3 to exit game.");
-
-                ConsoleKeyInfo result = Console.ReadKey();
-                if (result.Key == ConsoleKey.D1)
-                    gameRun = true;
-                if (result.Key == ConsoleKey.D3)
-                    return;
-                Console.Clear();
+                Console.WriteLine("Press 1 for: A*. \nPress 2 for Dijkstra. \nPress 3 to exit game.");
 
                 //New game
                 map = new Map(10, 10);
                 wizard = new Wizard(map.nodes[8, 1]);
+
+                ConsoleKeyInfo result = Console.ReadKey();
+                if (result.Key == ConsoleKey.D1)
+                {
+                    gameRun = true;
+                    wizard.aStar = true;
+                    wizard.dijkstra = false;
+                }
+                if (result.Key == ConsoleKey.D2)
+                {
+                    gameRun = true;
+                    wizard.aStar = false;
+                    wizard.dijkstra = true;
+                }
+                if (result.Key == ConsoleKey.D3)
+                    return;
+                Console.Clear();
                 map.Render(false);
             }
         }
--- aStarLooksForKeys/aStarLooksForKeys/Pathfinding.cs
+++ aStarLooksForKeys/aStarLooksForKeys/Pathfinding.cs
@@ -174,7 +174,169 @@ namespace aStarLooksForKeys
             return pathQ;
         }
 
+        static public Queue<Node> DijkstraQueue(Node start, Node goal, ref Map grid)
+        {
+            //Finds the path
+            List<Node> path = Pathfinding.Dijkstra(start, goal, ref grid);
 
+            //Queue
+            Queue<Node> pathQ = new Queue<Node>();
+
+            for (int i = 0; i < path.Count(); i++)
+            {
+                pathQ.Enqueue(path[i]);
+            }
+
+            return pathQ;
+        }
+
+        static public List<Node> Dijkstra(Node start, Node goal, ref Map grid)
+        {
+            List<Node> open = new List<Node>();
+            List<Node> closed = new List<Node>();
+
+            open.Add(start);
+
+            //Finds the cell from the open list, which has the lowest f(x) value. This cell is moved to the closed list..
+            Node current = open[0];
+
+            //Directional multiplicator.
+            int directionMulti;
+            //The values for moving linear or cross
+            int linear = 10;
+            int cross = 14;
+
+            //colors for the different cells, coorosponding to how the pathfinder looks at them
+            ConsoleColor openColor = ConsoleColor.Blue;
+            ConsoleColor closedColor = ConsoleColor.Red;
+            ConsoleColor pathColor = ConsoleColor.DarkBlue;
+
+            while (open.Count() > 0 && (current = open[0]) != goal)
+            {
+                //Adds the current to the closed list and removes it from the open list.
+                closed.Add(current);
+                open.RemoveAt(0);
+                //Sets color
+                current.colorPathfinding = closedColor;
+
+                //Runs through all the surrounding cells.
+                for (int x = -1; x <= 1; x++)
+                {
+                    for (int y = -1; y <= 1; y++)
+                    {
+                        int xCord = current.position.X + x;
+                        int yCord = current.position.Y + y;
+                        if (xCord < 0)
+                            xCord = 0;
+                        if (yCord < 0)
+                            yCord = 0;
+                        if (xCord >= grid.nodes.GetLength(0) - 1)
+                            xCord = grid.nodes.GetLength(0) - 1;
+                        if (yCord >= grid.nodes.GetLength(1) - 1)
+                            yCord = grid.nodes.GetLength(1) - 1;
+
+                        Node cellTemp = grid.nodes[xCord, yCord];
+
+                        //If the cell is not walkable or is in the closed list, then it's ignored.
+                        if (cellTemp.myType != MyType.notWalkable && !closed.Contains(cellTemp) && cellTemp.monsterHere != true)
+                        {
+                            //If the cell isn't in the open list, then it's added.
+                            if (!open.Contains(cellTemp))
+                            {
+                                open.Add(cellTemp);
+#if DEBUG
+                                //Sets the color
+                                cellTemp.colorPathfinding = openColor;
+#endif
+                                //The current cell is made the parent cell.
+                                cellTemp.parent = current;
+
+                                //G value is calculated.
+                                if ((x == -1 && y == -1) || (x == -1 && y == 1) || (x == 1 && y == -1) || (x == 1 && y == 1))
+                                    directionMulti = cross;
+                                else
+                                    directionMulti = linear;
+                                //The g value
+                                int gTemp = cellTemp.g * directionMulti;
+
+                                //The path value
+                                cellTemp.pathValue = current.pathValue + gTemp;
+                                //Sets a temp g value on the cell
+                                cellTemp.gTemp = cellTemp.g * directionMulti;
+
+                                //H value is calculaed.
+                                cellTemp.h = 0;
+                            }
+                            else
+                            {
+                                //G value is calculated.
+                                if ((x == -1 && y == -1) || (x == -1 && y == 1) || (x == 1 && y == -1) || (x == 1 && y == 1))
+                                    directionMulti = cross;
+                                else
+                                    directionMulti = linear;
+                                //The g value
+                                int gTemp = cellTemp.g * directionMulti;
+
+                                if (gTemp < cellTemp.gTemp)
+                                {
+                                    //The path value
+                                    cellTemp.pathValue = current.pathValue + cellTemp.g * directionMulti;
+                                    //Sets a temp g value on the cell
+                                    cellTemp.gTemp = cellTemp.g * directionMulti;
+
+                                    //H value is calculaed.
+                                    cellTemp.h = 0;
+
+#if DEBUG
+                                    //Sets the color
+                                    cellTemp.colorPathfinding = openColor;
+#endif
+                                    //The current cell is made the parent cell.
+                                    cellTemp.parent = current;
+                                }
+                            }
+                        }
+                    }
+                }
+                //Sorts the open list so the cell with the largest F value is at index 0.
+                InsertionSort(ref open);
+
+#if DEBUG
+                //Only for bug testing
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                int millisecondsToWait = 500;
+                grid.Render(true);
+                while (true)
+                {
+                    //some other processing to do STILL POSSIBLE
+                    if (stopwatch.ElapsedMilliseconds >= millisecondsToWait)
+                    {
+                        break;
+                    }
+                    Thread.Sleep(1); //so processor can rest for a while
+                }
+#endif
+            }
+
+            //Finds the path
+            List<Node> path = new List<Node>();
+            bool run = true;
+            while (run)
+            {
+#if DEBUG
+                //Sets color
+                current.colorPathfinding = pathColor;
+#endif
+                path.Add(current);
+                if (current.parent == null)
+                    break;
+                current = current.parent;
+                if (current.wizardHere)
+                    break;
+            }
+            path.Reverse();
+            return path;
+        }
 
         /// <summary>
         /// Sorts the list after the cells F value..
